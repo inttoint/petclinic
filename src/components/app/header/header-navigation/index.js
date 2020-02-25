adde187064cb1fe43ba5e9dfbf35f8458d4d97c3
Alina Voronina <skorokhodova.a5@gmail.com>
2020-02-25 23:48:52 +0300

    Выделение компонента под навигацию

---
+++ src/components/app/header/header-navigation/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../noun_primary50_.png';
+import './header-navigation.scss';
+
+const HeaderNavigation = () => {
+  return (
+    <header className="header">
+      <div className="header__logo">
+        <Link to="#" className="header__logo--link">
+          <img src={logo} alt="" />
+        </Link>
+      </div>
+      <div className="header__burger">
+        <span />
+        <span />
+        <span />
+      </div>
+      <nav className="header__menu">
+        <ul>
+          <li>
+            <Link to="#" className="header__menu--link">
+              О нас
+            </Link>
+          </li>
+          <li>
+            <Link to="#" className="header__menu--link">
+              Услуги
+            </Link>
+          </li>
+          <li>
+            <Link to="#" className="header__menu--link">
+              Отзывы
+            </Link>
+          </li>
+          <li>
+            <Link to="#" className="header__menu--link">
+              Контакты
+            </Link>
+          </li>
+          <li>
+            <button className="btn red-btn">Вход</button>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default HeaderNavigation;
--- src/components/app/header/index.js
+++ src/components/app/header/index.js
@@ -1,51 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './header.scss';
-import logo from './noun_primary50_.png';
+import HeaderNavigation from './header-navigation';
 
 const Header = () => {
   return (
     <div className="bg-full">
       <div className="inner-shadow">
-        <header className="header">
-          <div className="header__logo">
-            <Link to="#" className="header__logo--link">
-              <img src={logo} alt="" />
-            </Link>
-          </div>
-          <div className="header__burger">
-            <span />
-            <span />
-            <span />
-          </div>
-          <nav className="header__menu">
-            <ul>
-              <li>
-                <Link to="#" className="header__menu--link">
-                  О нас
-                </Link>
-              </li>
-              <li>
-                <Link to="#" className="header__menu--link">
-                  Услуги
-                </Link>
-              </li>
-              <li>
-                <Link to="#" className="header__menu--link">
-                  Отзывы
-                </Link>
-              </li>
-              <li>
-                <Link to="#" className="header__menu--link">
-                  Контакты
-                </Link>
-              </li>
-              <li>
-                <button className="btn red-btn">Вход</button>
-              </li>
-            </ul>
-          </nav>
-        </header>
+        <HeaderNavigation />
         <div className="intro">
           <h1>Что такое по-настоящему хорошая ветеринарная клиника?</h1>
           <div className="divider" />
