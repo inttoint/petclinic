d727cec6477253dd69cbd423fe4bcfce5e370f9e
Alina Voronina <skorokhodova.a5@gmail.com>
2020-02-29 00:15:44 +0300

    Добавлен прелоадер

--- src/components/pages/SignInPage.js
+++ src/components/pages/SignInPage.js
@@ -4,13 +4,18 @@ import catWithMac from '../../img/PajdTsgda.jpg';
 import SignInForm from '../forms/sign-in-form';
 import { signInUserRequest } from '../../redux/ac';
 import { connect } from 'react-redux';
+import { authLoadingSelector } from '../../redux/selectors';
+import Spinner from '../common/spinner';
 
 class SignInPage extends Component {
   render() {
+    const { isLoading } = this.props;
+
     return (
       <div>
         <Header backgroundImage={catWithMac}>
           <SignInForm handleSubmit={this.handleSignIn} />
+          {isLoading && <Spinner />}
         </Header>
       </div>
     );
@@ -19,4 +24,9 @@ class SignInPage extends Component {
   handleSignIn = user => this.props.signInUserRequest(user);
 }
 
-export default connect(null, { signInUserRequest })(SignInPage);
+export default connect(
+  state => ({
+    isLoading: authLoadingSelector(state)
+  }),
+  { signInUserRequest }
+)(SignInPage);
--- src/redux/selectors/auth.js
+++ src/redux/selectors/auth.js
@@ -1,3 +1,4 @@
 import { moduleName } from '../reducers/auth';
 
 export const isUserAuthorizedSelector = state => state[moduleName].isAuthorized;
+export const authLoadingSelector = state => state[moduleName].isLoading;
