5590b68de8d1e31ad55e2f8f0e99313fe0563c4a
Alina Voronina <skorokhodova.a5@gmail.com>
2020-03-01 13:13:11 +0300

    header-intro теперь переиспользуемый компонент

--- src/components/app/header/header-intro/index.js
+++ src/components/app/header/header-intro/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import './header-intro.scss';
 
-const HeaderIntro = () => {
+const HeaderIntro = ({ firstTitle, secondTitle }) => {
   return (
     <div className="intro">
-      <h1>Что такое по-настоящему хорошая ветеринарная клиника?</h1>
+      <h1>{firstTitle}</h1>
       <div className="divider" />
-      <h5>
-        Это место, в котором вы всегда можете рассчитывать на эффективную помощь
-        специалиста, квалифицированную консультацию и грамотное лечение
-        животного
-      </h5>
+      <h5>{secondTitle}</h5>
     </div>
   );
 };
--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -2,12 +2,16 @@ import React from 'react';
 import whiteCat from '../../img/britan.jpg';
 import Header from '../app/header';
 import HeaderIntro from '../app/header/header-intro';
+import titles from '../../mocks/titles';
 
 const HomePage = () => {
+  const { firstTitle, secondTitle } = titles.find(
+    title => title.location === 'home'
+  );
   return (
     <div>
       <Header backgroundImage={whiteCat}>
-        <HeaderIntro />
+        <HeaderIntro firstTitle={firstTitle} secondTitle={secondTitle} />
       </Header>
     </div>
   );
