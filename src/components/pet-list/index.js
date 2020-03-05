0bce01b38eee8544ed06075fac3f7b888b804c3f
Alina Voronina <skorokhodova.a5@gmail.com>
2020-03-05 23:38:44 +0300

    Компоненты для раздела Мои животные

---
+++ src/components/pet-list/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './pet-card.scss';
+
+const PetList = () => {
+  return (
+    <div>
+      <h1>pets</h1>
+    </div>
+  );
+};
+
+export default PetList;
---
+++ src/components/pet-list/pet-card/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './pet-card.scss';
+
+const PetCard = () => {
+  return (
+    <div>
+      <h1>card</h1>
+    </div>
+  );
+};
+
+export default PetCard;
