8442ea0abc1ed48f91fc8d665061c1dacc86f643
Alina Voronina <skorokhodova.a5@gmail.com>
2020-03-07 00:15:30 +0300

    Карточка животного

--- src/components/pet-list/pet-card/index.js
+++ src/components/pet-list/pet-card/index.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import avatar from './Avatar_cat.png';
 import './pet-card.scss';
 
 const PetCard = () => {
+  const [isOpen, toggleCardActions] = useState(false);
+
+  const activeClass = isOpen ? 'btn active' : 'btn';
+  const openClass = isOpen ? 'box open' : 'box';
+
   return (
     <div>
-      <h1>card</h1>
+      <h1>Мои животные</h1>
+      <div className="profile">
+        <div className="photo">
+          <img src={avatar} alt="" />
+        </div>
+        <div className="content">
+          <div className="text">
+            <h3>Василий</h3>
+            <h6>9 месяцев</h6>
+          </div>
+          <div
+            className={activeClass}
+            onClick={() => toggleCardActions(!isOpen)}>
+            <span />
+          </div>
+        </div>
+        <div className={openClass}>
+          <i className="fa fa-codepen" />
+          <i className="fa fa-facebook" />
+          <i className="fa fa-github" />
+          <i className="fa fa-tumblr" />
+          <i className="fa fa-twitter" />
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import IconBox from './icon-box';
 import TextContent from './text-content';
 import './profile.scss';
+import PetCard from '../pet-list/pet-card';
 
 const Profile = () => {
   return (
     <div className="wrapper">
       <div className="main-controls">
         <div className="main-controls__left">
-          <TextContent />
+          {/*<TextContent />*/}
+          <PetCard />
         </div>
         <div className="main-controls__right">
           <div className="outer-shadow">
