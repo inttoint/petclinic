91c5822876a985a72843cba8d0da4b79da7bd9b8
Alina Voronina <skorokhodova.a5@gmail.com>
2020-03-15 13:19:09 +0300

    Вынесла сброс пароля в отдельную форму

---
+++ src/components/forms/password-reset-form/index.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import './edit-profile-form.scss';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import * as Yup from 'yup';
+
+const PasswordResetForm = ({ handleSubmit }) => {
+  const passwordResetValidation = Yup.object({
+    password: Yup.string().min(6, 'Минимальная длина пароля - 6 символов'),
+    passwordConfirm: Yup.string().oneOf(
+      [Yup.ref('password'), null],
+      'Пароли не совпадают'
+    )
+  });
+
+  return (
+    <div className="edit-profile">
+      <Formik
+        initialValues={{
+          password: '',
+          passwordConfirm: ''
+        }}
+        validationSchema={passwordResetValidation}
+        onSubmit={handleSubmit}>
+        {({ isValid, dirty }) => (
+          <Form className="password-reset-form">
+            <h4>Изменить пароль:</h4>
+            <div className="password-reset-form__group">
+              <Field
+                name="password"
+                type="password"
+                placeholder="Новый пароль"
+              />
+              <ErrorMessage
+                name="password"
+                className="invalid-feedback"
+                component="div"
+              />
+            </div>
+            <div className="password-reset-form__group">
+              <Field
+                name="passwordConfirm"
+                type="password"
+                placeholder="Повторите пароль"
+              />
+              <ErrorMessage
+                name="passwordConfirm"
+                className="invalid-feedback"
+                component="div"
+              />
+            </div>
+
+            <button
+              className="btn save"
+              type="submit"
+              disabled={!(isValid && dirty)}>
+              Сохранить
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
+
+export default PasswordResetForm;
